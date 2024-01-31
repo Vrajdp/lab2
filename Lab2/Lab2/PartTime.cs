f0d9cc06983f672c67b69a0f03181d2ba659f8ce
Vrajdp <152833390+Vrajdp@users.noreply.github.com>
2024-01-31 16:39:15 -0700

    Add files via upload

---
+++ Lab2/Lab2/PartTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Lab2
+{
+    // PartTime class inheriting from Employee class
+    public class PartTime : Employee
+    {
+        // Properties for hours and rate
+        public double Hours { get; set; }
+        public double Rate { get; set; }
+
+        // Constructor to initialize fields using the base class constructor
+        public PartTime(string id, string name, string address, string phone, long sin, string dateOfBirth, string department, double hours, double rate)
+            : base(id, name, address, phone, sin, dateOfBirth, department)
+        {
+            Hours = hours;
+            Rate = rate;
+        }
+
+        // Method to calculate and return the payment for a part-time employee
+        public double GetPay()
+        {
+            double salary = 0;
+
+            // Check if the hours worked are within regular working hours (40 hours)
+            if (Hours <= 40)
+                salary = Hours * Rate;  // Calculate regular pay
+            else
+                salary = 40 * Rate + (Hours - 40) * (Rate * 1.5);  // Calculate pay with overtime
+
+            return salary;
+        }
+    }
+}
---
+++ Lab2/Lab2/Wages.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab2
+{
+    // Wages class inheriting from Employee class
+    public class Wages : Employee
+    {
+        // Fields for hours (private) and rate
+        private int hours;
+
+        public double Hours { get; set; }
+        public double Rate { get; set; }
+
+        // Constructor to initialize fields using the base class constructor
+        public Wages(string id, string name, string address, string phone, long sin, string dateOfBirth, string department, double hours, double rate)
+            : base(id, name, address, phone, sin, dateOfBirth, department)
+        {
+            Hours = hours;
+            Rate = rate;
+        }
+
+        // Method to calculate and return the payment for an employee paid on an hourly basis
+        public double GetPay()
+        {
+            double salary = 0;
+
+            // Check if the hours worked are within regular working hours (40 hours)
+            if (Hours <= 40)
+                salary = Hours * Rate;  // Calculate regular pay
+            else
+            {
+                int overtimeHours = (int)(hours - 40);
+                salary = (overtimeHours * (Rate * 1.5)) + (40 * Rate);  // Calculate pay with overtime
+            }
+
+            return salary;
+        }
+    }
+}
